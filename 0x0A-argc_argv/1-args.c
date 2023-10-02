56ec21fe9ed0f3c3f02f88ed071402405309a3a5
MarthaweKwa <chaitezvimartha2000@gmail.com>
2023-10-02 16:55:22 -0700

    Task2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-#include "stdio.h"
 
 /**
  * main - prints the name of the program
@@ -11,9 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
+	if (argc == 1)
 	{
-		printf("%s\n", argv[0]);
+		printf("0\n");
 	}
-	return (0);
+	else
+	{
+		printf("%d\n", argc-1);
+	}
+	return(0)
 }
