7657f896638287039ce66a8c8b3f83574d0e7bad
MarthaweKwa <chaitezvimartha2000@gmail.com>
2023-10-07 18:03:19 -0700

    Task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,9 +5,10 @@
  * *_memset - a function that fills memory with a constant byte
  * @s: memory area
  * @b: char to copy
- * @n: number of times to copy b
- * Return: pointer 
+ * @n: number of times to copy
+ * Return: pointer
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int j;
